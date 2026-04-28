83c32a195672bc886c1f6358213f7e819b78d2d1
MrAshS <59808006+MrAshS@users.noreply.github.com>
2026-04-28 00:46:31 -0400

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,11 @@
+def farewell(name):
+    print("Goodbye, Bob")
+    print("Goodbye, Charlie")
+    result = "Goodbye, " + name
+    print(result)
+
+
+
+
+# don't modify below this line
+farewell("NeetCode")
